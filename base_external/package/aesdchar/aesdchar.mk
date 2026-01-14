e899364ff87777c3c96f39add5919ad465e13579
Antonio Almenara <aalmenara@oesia.com>
2026-01-14 09:28:04 +0100

    First commit for assignment 8

---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,28 @@
+
+##############################################################
+#
+# AESDCHAR
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_ASSIGNMENTS_VERSION = 'cd0836e529a655f3f78e636f466691cbe09e38a8'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_SITE = 'git@github.com:antonioalm45/LSPBAssignment3.git'
+AESDCHAR_ASSIGNMENTS_SITE_METHOD = git
+AESDCHAR_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+# ------------- Parte de kernel module (out-of-tree) -------------
+# Indica el/los subdirectorios donde hay un Makefile de módulo (Kbuild)
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
+# Evalúa la infraestructura de módulos de kernel
+
+# Hook Buildroot's helpers: first the kernel-module infra, then a package infra
+$(eval $(kernel-module))
+$(eval $(generic-package))
+
+
+
